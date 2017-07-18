ad06275c37fe9e849a58a629c7605899d452d89e
tools <1041988365@qq.com>
2017-07-18 19:27:29 +0800

    1 展示任务列表

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	// 创建angularjs模块
+	angular
+	.module("todoApp",[])
+	.controller("TodoController",["$scope",TodoController]);
+
+	function TodoController($scope){
+		// vm ---> viewmodel 视图模型
+		var vm = $scope;
+
+		// 1 展示任务列表
+		// 定义数据
+		var todoList=[
+			{id:1,name:"刘备",isCompleted:false},
+			{id:2,name:"关羽",isCompleted:false},
+			{id:3,name:"张飞",isCompleted:true},
+			{id:4,name:"赵云",isCompleted:false}
+		];
+
+		vm.todoList=todoList;
+
+		
+
+	}
 
 })(window);
